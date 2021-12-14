1d042a3ac37c53dad7d23945a80803cd4235a323
Loud Coypu <loud.coypu-code@yahoo.com>
2021-12-14 14:39:21 +0100

    vanill html form using hooks

--- curcumy-web/src/views/EditPosition.js
+++ curcumy-web/src/views/EditPosition.js
@@ -1,153 +1,97 @@
-import React, { Component } from "react";
-import PositionDataService from "../services/position.service"
-import { Link } from "react-router-dom";
 import {
-    FormControl,
-    FormLabel,
-    Input,
-    Radio,
-    RadioGroup,
     HStack,
-    // FormErrorMessage,
-    FormHelperText,
-    Button
-} from '@chakra-ui/react';
+    Stack,
+    Stat,
+    StatHelpText,
+    StatLabel
+} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import PositionDataService from "../services/position.service"
 
-class EditPosition extends Component {
-    constructor(props) {
-        super(props);
 
-        // setting up function
-        this.onChangePortfolio = this.onChangePortfolio.bind(this);
-        this.onChangeProtocol = this.onChangeProtocol.bind(this);
-        this.onChangeAsset = this.onChangeAsset.bind(this);
-        this.onChangeAssetName = this.onChangeAssetName.bind(this);
-        this.onChangeAssetType = this.onChangeAssetType.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+export default function EditPosition(props) {
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, reset } = useForm();
 
-        // setting up state
-        this.state = {
-            _is: null,
-            portfolio: '',
-            protocol: '',
-            asset: '',
-            assetName: '',
-            assetType: ''
-        }
-    }
+    // user state for form
+    const [position, setPosition] = useState(null);
 
-    onChangePortfolio(e) {
-        this.setState({ portfolio: e.target.value })
-    }
-
-    onChangeProtocol(e) {
-        this.setState({ protocol: e.target.value })
-    }
-
-    onChangeAsset(e) {
-        this.setState({ asset: e.target.value })
-    }
-
-    onChangeAssetName(e) {
-        this.setState({ assetName: e.target.value })
-    }
-
-    onChangeAssetType(e) {
-        this.setState({ assetType: e.target.value })
-    }
-    
-
-    componentDidMount() {
-        if (this.props.match.params && this.props.match.params.id) {
-            if (this.props.match.params.id !== "*") {
-                PositionDataService.get(this.props.match.params.id)
-                    .then(res => {
-                        this.setState(res.data.data);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    })
+    const retrieveData = () => {
+        if (props.match.params && props.match.params.id) {
+            if (props.match.params.id !== "*") {
+                return PositionDataService.get(props.match.params.id);
             }
         }
     }
 
+    // effect runs on component mount
+    useEffect(() => {
+        retrieveData()
+            .then(res => {
+                setPosition(res.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
+    // effect runs when user state is updated
+    useEffect(() => {
+        // reset form with user data
+        reset(position);
+    }, [position]);
 
-    onSubmit(e) {
-        e.preventDefault();
-
-        if (this.props.match.params) {
-
-            PositionDataService.update(this.props.match.params.id, this.state)
-                .then(res => {
-                    if (res.status === 500) {
-                        console.log(res.data);
-                    } else if (res.status === 200 && res.data.success === true) {
-                        console.log(res.data.id);
-                    } else if (res.status === 200 && res.data.success !== true) {
-                        console.log("Error updated new data because : " + res.data.error);
-                    } else {
-                        console.log("Server error with : " + res.data);
-                    }
-                }).catch(err => console.warn(err));
-        }
-        else {
-            PositionDataService.create(this.state)
-                .then(response => {
-                    if (response.status === 500) {
-                        console.log(response.data);
-                    } else if (response.status === 200 && response.data.success === true) {
-                        console.log(response.data.id);
-                    } else if (response.status === 200 && response.data.success !== true) {
-                        console.log("Error inserted new data because : " + response.data.error);
-                    } else {
-                        console.log("Server error with : " + response.data);
-                    }
-                })
-                .catch(err => console.warn(err));
-        }
-        this.props.history.push('/positions');
+    function onSubmit(data) {
+        // display form data on submit
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(data, null, 4));
+        return false;
     }
 
-    render() {
-
-        return (
-            <form onSubmit={this.onSubmit}>
-                <FormControl id="Portfolio">
-                    <FormLabel>Portfolio :</FormLabel>
-                    <Input type="text" value={this.state.portfolio} onChange={this.onChangePortfolio} />
-                </FormControl>
-
-                <FormControl id="Protocol">
-                    <FormLabel>Protocol :</FormLabel>
-                    <Input type="text" value={this.state.protocol} onChange={this.onChangeProtocol} />
-                </FormControl>
-
-                <FormControl id="Asset">
-                    <FormLabel>Asset :</FormLabel>
-                    <Input type="text" value={this.state.asset} onChange={this.onChangeAsset} />
-                </FormControl>
-
-                <FormControl id="AssetName">
-                    <FormLabel>Asset Name :</FormLabel>
-                    <Input type="text" value={this.state.assetName} onChange={this.onChangeAssetName} />
-                </FormControl>
-
-                <FormControl id="AssetType">
-                    <FormLabel>Asset Type :</FormLabel>
-                    <RadioGroup value={this.state.assetType} onChange={this.onChangeAssetType} defaultValue="token">
-                        <HStack>
-                            <Radio value="token">Token</Radio>
-                            <Radio value="pool">Pool</Radio>
-                        </HStack>
-                    </RadioGroup>
-                    <FormHelperText>Select if the asset is a simple token or a token pool / liquidity pool.</FormHelperText>
-                </FormControl>
-                <Button size="lg" type="submit">Save</Button>
-                <Link to={"/positions"}>Cancel</Link>
-            </form>
-        );
-    }
+    return (
+        <div className="card m-3">
+            <h5 className="card-header">React Hook Form - Set Form Values in useEffect Example</h5>
+            <div className="card-body">
+                {position &&
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group col-5">
+                            <label>Portfolio</label>
+                            <input name="portfolio" type="text" {...register('portfolio')} className="form-control" />
+                        </div>
+                        <div className="form-group col-5">
+                            <label>Protocol</label>
+                            <input name="protocol" type="text" {...register('protocol')} className="form-control" />
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-5">
+                                <label>Asset</label>
+                                <input name="asset" type="text" {...register('asset')} className="form-control" />
+                            </div>
+                            <div className="form-group col-5">
+                                <label>Asset name</label>
+                                <input name="assetName" type="text" {...register('assetName')} className="form-control" />
+                            </div>
+                        </div>
+                        <div className="form-group col">
+                            <label>Asset Type</label>
+                            <select name="title" {...register('assetType')} className="form-control">
+                                <option value=""></option>
+                                <option value="token">Token</option>
+                                <option value="pool">Pool</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary mr-1">Submit</button>
+                            <button type="button" onClick={() => reset()} className="btn btn-secondary">Reset</button>
+                        </div>
+                    </form>
+                }
+                {!position &&
+                    <div className="text-center p-3">
+                        <span className="spinner-border spinner-border-lg align-center"></span>
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
-
-export default EditPosition
